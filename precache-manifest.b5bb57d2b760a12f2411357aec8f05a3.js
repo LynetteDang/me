e37a250ccc088657e0b93624bb736437e49a2216
Lynette Dang <lidang@uchicago.edu>
2022-06-08 14:02:08 -0700

    Updates

---
+++ precache-manifest.b5bb57d2b760a12f2411357aec8f05a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d49ace78193cd15d06af",
+    "url": "/me/static/css/main.181f977a.chunk.css"
+  },
+  {
+    "revision": "d49ace78193cd15d06af",
+    "url": "/me/static/js/main.d49ace78.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9f153e9cc8c8fe252869b5862bdb7c24",
+    "url": "/me/index.html"
+  }
+];
